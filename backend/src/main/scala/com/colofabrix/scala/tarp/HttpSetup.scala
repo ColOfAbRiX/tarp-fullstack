8481850e7535ee65248e94a2abc5188b5e5143e0
Fabrizio Colonna <colofabrix@tin.it>
2024-12-22 23:53:29 +0100

    Basic structure

---
+++ backend/src/main/scala/com/colofabrix/scala/tarp/HttpSetup.scala
@@ -0,0 +1,53 @@
+package com.colofabrix.scala.tarp
+
+import cats.effect.IO
+import cats.implicits.*
+import com.comcast.ip4s.*
+import org.http4s.ember.server.EmberServerBuilder
+import org.http4s.server.middleware.{ Logger => LoggerMiddleware }
+import org.http4s.server.middleware.CORS
+import org.typelevel.log4cats.Logger
+import org.typelevel.log4cats.slf4j.Slf4jLogger
+import org.typelevel.log4cats.syntax.LoggerInterpolator
+import sttp.tapir.server.http4s.Http4sServerInterpreter
+import sttp.tapir.swagger.bundle.SwaggerInterpreter
+
+object HttpSetup:
+
+  implicit private val logger: Logger[IO] =
+    Slf4jLogger.getLogger()
+
+  def setup(): IO[Unit] =
+    val crossOriginResourceSharingPolicy =
+      CORS
+        .policy
+        .withAllowOriginHostCi(_ => com.colofabrix.scala.tarp.TarpConfig.config.devMode)
+
+    val httpApp =
+      crossOriginResourceSharingPolicy(allRoutes)
+
+    EmberServerBuilder
+      .default[IO]
+      .withHost(ipv4"0.0.0.0")
+      .withPort(com.colofabrix.scala.tarp.TarpConfig.config.http.port)
+      .withHttpApp(httpApp.orNotFound)
+      .build
+      .use(_ => IO.never)
+      .start
+      .void
+
+  private val docsEntpoint =
+    SwaggerInterpreter().fromEndpoints[IO](List.empty, "Backend - TARP Stack", "1.0")
+
+  private val allRoutes =
+    val loggerMiddleware =
+      LoggerMiddleware
+        .httpRoutes(
+          logHeaders = true,
+          logBody = true,
+          redactHeadersWhen = _ => !com.colofabrix.scala.tarp.TarpConfig.config.devMode,
+          logAction = Some((msg: String) => info"$msg"),
+        )
+        .apply(_)
+
+    loggerMiddleware(Http4sServerInterpreter[IO]().toRoutes(docsEntpoint))
